100cf98c95fc18bf69368089886f77f1a219f6d1
alchemicAV <35404595+alchemicAV@users.noreply.github.com>
2025-02-28 17:36:35 -0500

    Add files via upload

---
+++ src/web-vitals.js
@@ -0,0 +1,9 @@
+export default function() {
+  // Do nothing
+}
+
+export const getCLS = () => {};
+export const getFID = () => {};
+export const getFCP = () => {};
+export const getLCP = () => {};
+export const getTTFB = () => {};
