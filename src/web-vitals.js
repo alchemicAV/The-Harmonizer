ca141589a05b62c3db67eba53a5496cfcd2240e3
alchemicAV <35404595+alchemicAV@users.noreply.github.com>
2025-02-28 17:38:13 -0500

    Delete src/web-vitals.js

--- src/web-vitals.js
+++
@@ -1,9 +0,0 @@
-export default function() {
-  // Do nothing
-}
-
-export const getCLS = () => {};
-export const getFID = () => {};
-export const getFCP = () => {};
-export const getLCP = () => {};
-export const getTTFB = () => {};
